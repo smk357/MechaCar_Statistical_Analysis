fccc04b3bf03bd1027e6db53a1cab71a7abfe405
Syed Murtaza Kazmi <syed.murtaza.kazmi@gmail.com>
2021-07-22 17:02:07 -0400

    adding deliverable 1

---
+++ MechaCarChallenge.R
@@ -0,0 +1,5 @@
+library(dplyr)
+mechacar_df <- read.csv(file='MechaCar_mpg.csv')
+
+lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=mechacar_df)
+summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=mechacar_df))
