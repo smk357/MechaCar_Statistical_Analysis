c9a4736ae8e46de3ce5dc045c2831598be2edcc0
Syed Murtaza Kazmi <syed.murtaza.kazmi@gmail.com>
2021-07-22 18:00:08 -0400

    adding deliverable 3

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -12,3 +12,18 @@ suspension_coil_table <- read.csv(file='Suspension_Coil.csv', check.names=F,stri
 
 total_summary <- suspension_coil_table%>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), Standard_Deviation=sd(PSI), .groups = 'keep')
 lot_summary <- suspension_coil_table%>% group_by(Manufacturing_Lot)%>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), Standard_Deviation=sd(PSI), .groups = 'keep')
+
+## Deliverable 3.1
+
+t.test((suspension_coil_table$PSI), mu=1500)
+
+## Deliverable 3.2
+
+## lo1 1
+t.test(x=(subset(suspension_coil_table, Manufacturing_Lot=='Lot1'))$PSI, mu=1500)
+
+## lot 2
+t.test(x=(subset(suspension_coil_table, Manufacturing_Lot=='Lot2'))$PSI, mu=1500)
+
+## lot 3
+t.test(x=(subset(suspension_coil_table, Manufacturing_Lot=='Lot3'))$PSI, mu=1500)
